ba88ccdefafb56961bded1fbb832be92fe3be444
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-11-17 22:06:38 -0500

    ref: app.py add AI response format

--- server/app.py
+++ server/app.py
@@ -1,4 +1,51 @@
+import os
+import sys
+from constants import OPENROUTER_API_URL, OPENROUTER_API_KEY
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from openai import OpenAI, OpenAIError
+
+client = OpenAI(
+    base_url=OPENROUTER_API_URL,
+    api_key=OPENROUTER_API_KEY,
+)
+
+# Introduction Section
+st.write("# Theft Over Open Data - Analysis and Visualization")
+
+
+# Data Exploration Section with Streamlit
+
+# Geographical Visualization and Mapping Section
+
+# LogisticRegression Model & Tree Visualization Section
+
+# Model Evaluation Section
+
+# AI Evaluation and Explanation Section
+@st.cache
+async def get_explanation() -> str:
+    response = await client.chat.completions.create(
+        model="google/gemini-flash-1.5-exp",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "<enter a descriptive user msg here>"
+            },
+        ]
+    )
+    return response.choices[0].message.content
+
+
+def run_app() -> None:
+    os.system("streamlit run app.py --server.port 8501")
 
 
 if __name__ == "__main__":
-    pass
+    run_app()
--- server/constants.py
+++ server/constants.py
@@ -15,3 +15,8 @@ load_dotenv(find_dotenv())
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENROUTER_API_URL = os.getenv("OPENROUTER_API_URL")
+
+MODEL_PROMPT = """"
+<enter a descriptive model prompt here>
+"""
+MODEL_NAME = "google/gemini-flash-1.5-exp"
